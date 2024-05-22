89d25bfedfb69c73762cb8200f7b741810053067
Fatihcagdas100 <154072241+Fatihcagdas100@users.noreply.github.com>
2024-05-22 10:25:28 +0300

    Primes...

---
+++ Primes_Between_Numbers/Primes_Between_Numbers/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Primes_Between_Numbers
+{
+    internal class Class1
+    {
+        public void asalBul(int baslangicS, int sonS)
+        {
+            if (baslangicS <= 1)
+                baslangicS = 2;
+            for (int i = sonS; i >= baslangicS; i--)
+            {
+                double kok = Math.Sqrt(i);
+                bool kontrol = true;
+                for (int j = 2; kontrol != false && j <= kok; j++)
+                {
+                    if (i % j == 0)
+                        kontrol = false;
+                }
+                if (kontrol == true)
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ Primes_Between_Numbers/Primes_Between_Numbers/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Primes_Between_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            Stopwatch inputStopwatch = new Stopwatch();
+            inputStopwatch.Start();
+
+            int baslangicS = Convert.ToInt32(Console.ReadLine());
+            int sonS = Convert.ToInt32(Console.ReadLine());
+            inputStopwatch.Stop();
+
+            TimeSpan inputTime = inputStopwatch.Elapsed;
+
+            Class1 class1 = new Class1();
+            Thread thread = new Thread(() => class1.asalBul(baslangicS, sonS));
+            thread.Start();
+            thread.Join();
+
+
+            stopwatch.Stop();
+
+            TimeSpan ts = stopwatch.Elapsed;
+            TimeSpan totalTime = ts - inputTime;
+
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                totalTime.Hours, totalTime.Minutes, totalTime.Seconds,
+                totalTime.Milliseconds / 10);
+            Console.WriteLine("Programın çalışma süresi (veri girişi hariç): " + elapsedTime);
+
+            Console.ReadLine();
+        
+        }
+    }
+}
